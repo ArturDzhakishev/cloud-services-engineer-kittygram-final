60859f7ae70cb43a46ddb901649881544a76b3b6
artur <arturdzhak@gmail.com>
2025-01-07 23:07:59 +0400

    correct autotest1.12

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,8 @@ DEPLOY_INFO_FILE_NAME = "tests.yml"
 KITTYGRAM_DOMAIN_KEY = "kittygram_domain"
 DOCKERFILE_NAME = "Dockerfile"
 DOCKERHUB_USERNAME_KEY = "dockerhub_username"
-#WORKFLOW_FILE = "kittygram_workflow.yml"
-WORKFLOW_FILE = "main.yml"
-DOCKER_COMPOSE_PROD_FILE_NAME = "docker-compose.yml"
-#DOCKER_COMPOSE_PROD_FILE_NAME = "docker-compose.production.yml"
+WORKFLOW_FILE = "kittygram_workflow.yml"
+DOCKER_COMPOSE_PROD_FILE_NAME = "docker-compose.production.yml"
 
 for dir_name in (BACKEND_DIR_NAME, FRONTEND_DIR_NAME, NGINX_DIR_NAME):
     path_to_dir = BASE_DIR / dir_name
